837a707d31d4c1d308cd3e8af1c24d3683d1933d
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-11 21:52:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,10 +25,5 @@ class Song
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
-# <<<<<<< HEAD
-#     self.artist.add_song(self)
-# =======
-#     artist.add_song(self)
-# >>>>>>> 182f658a9fa771b140a33311a4c91fa54e04a49e
   end
 end
