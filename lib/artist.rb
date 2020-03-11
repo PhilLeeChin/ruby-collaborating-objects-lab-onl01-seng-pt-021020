67c884bd94a9960762dd745fa2761cb7b1c31ea7
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-11 21:49:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,25 +25,25 @@ class Artist
   end
 
   def self.find(name)
-<<<<<<< HEAD
-    self.all.find {|artist| artist.name == artist.name}
-  end
-
-  def self.find_or_create_by_name(name)
-    self.find(name) ? self.find(name) : self.new(name)
-=======
-    self.all.find {|artist| artist.name == artist}
-  end
-
-  def self.create(name)
-    artist = Artist.new(name)
-    artist.save
-    artist
-  end
-
-  def self.find_or_create_by_name(name)
-    self.find(name) != nil ? self.find(name) : self.create(name)
->>>>>>> 182f658a9fa771b140a33311a4c91fa54e04a49e
+# <<<<<<< HEAD
+#     self.all.find {|artist| artist.name == artist.name}
+#   end
+# 
+#   def self.find_or_create_by_name(name)
+#     self.find(name) ? self.find(name) : self.new(name)
+# =======
+#     self.all.find {|artist| artist.name == artist}
+#   end
+# 
+#   def self.create(name)
+#     artist = Artist.new(name)
+#     artist.save
+#     artist
+#   end
+# 
+#   def self.find_or_create_by_name(name)
+#     self.find(name) != nil ? self.find(name) : self.create(name)
+# >>>>>>> 182f658a9fa771b140a33311a4c91fa54e04a49e
   end
 
   def print_songs
