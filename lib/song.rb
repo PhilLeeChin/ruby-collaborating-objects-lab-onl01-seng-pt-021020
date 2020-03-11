ddda21eab197de9dd32043111e6199e2a0ca8918
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-11 21:51:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Song
   attr_accessor :name, :artist
 
@@ -18,25 +19,16 @@ class Song
   end
 
   def self.new_by_filename(title)
-# <<<<<<< HEAD
-#     songs = title.split(" - ")[1]
-#     artist = title.split(" - ")[0]
-#     song = self.new(songs)
-#     song.artist_name = artist
-# =======
-#     songs = title.chomp(".mp3").split(" - ")
-#     song = Song.new(songs[1])
-#     song.artist = songs[0]
-# >>>>>>> 182f658a9fa771b140a33311a4c91fa54e04a49e
+    binding.pry
     song
   end
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
-<<<<<<< HEAD
-    self.artist.add_song(self)
-=======
-    artist.add_song(self)
->>>>>>> 182f658a9fa771b140a33311a4c91fa54e04a49e
+# <<<<<<< HEAD
+#     self.artist.add_song(self)
+# =======
+#     artist.add_song(self)
+# >>>>>>> 182f658a9fa771b140a33311a4c91fa54e04a49e
   end
 end
